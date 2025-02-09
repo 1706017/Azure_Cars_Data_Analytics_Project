a6e9f31808bb0189e7c08c58ccda791990331745
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-09 14:53:38 +0530

    01_db_notebook.py

---
+++ db_notebook.py
@@ -0,0 +1,18 @@
+%md
+ #Create Catalog
+
+%sql
+CREATE CATALOG cars_catalog;
+
+%md
+#Create Schema
+
+%sql
+CREATE SCHEMA cars_catalog.silver;
+
+CREATE SCHEMA cars_catalog.gold
+
+#here we have created two schemas one is silver and other is gold
+
+
+
